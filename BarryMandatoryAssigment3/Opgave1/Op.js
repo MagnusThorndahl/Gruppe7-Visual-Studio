5170b584fe4ee90f8bf58de612a188f027bdaea5
Magnus Thorndahl <magnus@thorndahl.com>
2025-11-17 19:23:49 +0100

    assignment3

---
+++ BarryMandatoryAssigment3/Opgave1/Op.js
@@ -0,0 +1,13 @@
+let rejse = [
+  { 
+    passagerNavn: "Barry",         // Variable names typically start with lowercase
+    startDestination: "Kongens Nytorv",  // slight correction to station spelling
+    slutDestination: "Nørreport St.",
+    checkIndTid: "2024-06-19T08:00:00",
+    checkUdTid: "2024-06-19T08:10:00",
+    rejsekortNummer: "RK1234567890",
+    rejsePris: "19 DKK"
+  }    
+];
+
+console.log(rejse);
