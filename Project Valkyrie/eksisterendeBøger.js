e509f5d082ae87e5abe399b5b772924e19efbfaf
Mathias Rasmussen <mathias14242300@gmail.com>
2025-12-05 11:21:09 +0100

    billeder

--- Project Valkyrie/eksisterendeBøger.js
+++ Project Valkyrie/eksisterendeBøger.js
@@ -1,10 +1,10 @@
 // Format = user/title/stand/pris/årstal/isbnnr/sprog/format/forfatter //
 let books = [
-  { user: "August", title: "Medier og Samfund", stand: "Ubrugt", pris: 100, årstal: 2008, isbnnr: "9788759313534", sprog: "Dansk", format: "Paperback", forfatter: "Bruhn Jensen, Klaus", image: "img/medier_og_samfund.jpg" },
-  { user: "Mathias", title: "Javascript Absolute Beginner's Guide", stand: "Som ny", pris: 125, årstal: 2023, isbnnr: "9780137959167", sprog: "Engelsk", format: "Paperback", forfatter: "Chinnathambi, Kirupa" },
-  { user: "Magnus", title: "HTML & CSS", stand: "Lettere brugt", pris: 110, årstal: 2011, isbnnr: "9781118008188", sprog: "Engelsk", format: "Paperback", forfatter: "Duckett, Jon" },
-  { user: "Emil", title: "Social Research", stand: "Ubrugt", pris: 150, årstal: 2016, isbnnr: "9780745671857", sprog: "Engelsk", format: "Paperback", forfatter: "Blaikie, Norman & Priest, Jan" },
-  { user: "Freja", title: "Adventure in Statistics", stand: "Lettere brugt", pris: 130, årstal: 2015, isbnnr: "9781609185994", sprog: "Engelsk", format: "Paperback", forfatter: "Peng, Andy & Ingersoll, Jeremy" },
-  { user: "Thea", title: "Theory and Reality", stand: "Som ny", pris: 140, årstal: 2010, isbnnr: "9780226480782", sprog: "Engelsk", format: "Paperback", forfatter: "Godfrey‑Smith, Peter" },
-  { user: "Sofie", title: "Data Science for Dummies", stand: "Ubrugt", pris: 120, årstal: 2021, isbnnr: "9781119646135", sprog: "Engelsk", format: "Paperback", forfatter: "Mueller, Lillian Pierson" }
+  { user: "August", title: "Medier og Samfund", stand: "Ubrugt", pris: 100, årstal: 2008, isbnnr: "9788759313534", sprog: "Dansk", format: "Paperback", forfatter: "Bruhn Jensen, Klaus", image: "https://i.imgur.com/q0ZMfJz.jpg" },
+  { user: "Mathias", title: "Javascript Absolute Beginner's Guide", stand: "Som ny", pris: 125, årstal: 2023, isbnnr: "9780137959167", sprog: "Engelsk", format: "Paperback", forfatter: "Chinnathambi, Kirupa", image: "https://i.imgur.com/UYifDU4.jpg"},
+  { user: "Magnus", title: "HTML & CSS", stand: "Lettere brugt", pris: 110, årstal: 2011, isbnnr: "9781118008188", sprog: "Engelsk", format: "Paperback", forfatter: "Duckett, Jon", image: https: "//i.imgur.com/9fy68nV.jpg" },
+  { user: "Emil", title: "Social Research", stand: "Ubrugt", pris: 150, årstal: 2016, isbnnr: "9780745671857", sprog: "Engelsk", format: "Paperback", forfatter: "Blaikie, Norman & Priest, Jan", image: "https://i.imgur.com/7GGju3L.jpg" },
+  { user: "Freja", title: "Adventure in Statistics", stand: "Lettere brugt", pris: 130, årstal: 2015, isbnnr: "9781609185994", sprog: "Engelsk", format: "Paperback", forfatter: "Peng, Andy & Ingersoll, Jeremy", image: "https://i.imgur.com/PnJ2e3V.jpg" },
+  { user: "Thea", title: "Theory and Reality", stand: "Som ny", pris: 140, årstal: 2010, isbnnr: "9780226480782", sprog: "Engelsk", format: "Paperback", forfatter: "Godfrey‑Smith, Peter", image: "https://i.imgur.com/NISwz2v.jpg" },
+  { user: "Sofie", title: "Data Science for Dummies", stand: "Ubrugt", pris: 120, årstal: 2021, isbnnr: "9781119646135", sprog: "Engelsk", format: "Paperback", forfatter: "Mueller, Lillian Pierson", image: "https://i.imgur.com/OJEbVTH.jpg"}
 ];
