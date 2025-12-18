3d401a854589a2d051d8b779b5092a0645fcf45d
Magnus Thorndahl <magnus@thorndahl.com>
2025-12-18 12:10:30 +0100

    updated

--- Project Valkyrie/fotoGalleri.js
+++ Project Valkyrie/fotoGalleri.js
@@ -1,25 +1,23 @@
-// vi definerer her som det første variablerne vi bruger i vores query til Pexels API'et blandt andet api nøgle og kollektionsid //
+// Vi definerer variablerne til vores API-kald: HTML-element, API-nøgle, kollektions-id og antal billeder
 let galleri = document.querySelector('.gallery-grid');
 let API_NØGLE = 'qT0vXrdjtj1XR6qkq5DtIeruJL8o3GuGeinzROqNUlFCTR81tE4r425v';
-let kollektionsId ='kriwv7u';
+let kollektionsId = 'kriwv7u';
 let sideAntal = 25; 
 
-// funktionen der henter fotos fra Pexels API'et og viser dem i galleriet i vores html fotogalleri.html //
-     async function loadFotos(sidenummer = 1) {    
-let URL = `https://api.pexels.com/v1/collections/${kollektionsId}?type=photos&per_page=${sideAntal}&page=${sidenummer}`;  
-let svar = await fetch(URL, { headers: { Authorization: API_NØGLE } });
-let data = await svar.json();
-let photos = data.media.filter(item => item.type === 'Photo');
+// Funktionen der henter fotos fra Pexels API'et og viser dem i HTML-galleriet
+async function loadFotos(sidenummer = 1) {    
+    let URL = `https://api.pexels.com/v1/collections/${kollektionsId}?type=photos&per_page=${sideAntal}&page=${sidenummer}`;  
+    let svar = await fetch(URL, { headers: { Authorization: API_NØGLE } });
+    let data = await svar.json();
+    let photos = data.media.filter(item => item.type === 'Photo');
 
     galleri.innerHTML = ''; 
 
-    // Looper igennem de fotos vi modtager igennem API'et og opretter et img element for hvert foto //
+    // Vi looper igennem de filtrerede fotos og opretter et img-element for hvert billede
     for (let Photo of photos) {
-      let image = document.createElement('img')
-      image.src = Photo.src.medium;
-      galleri.appendChild(image);
+        let image = document.createElement('img');
+        image.src = Photo.src.medium; //
+        galleri.appendChild(image);  
     }
-
-  }
-
+}
 loadFotos();
